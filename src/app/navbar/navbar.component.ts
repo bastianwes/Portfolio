80f07a1afd43b5cf6c9d001a352ca0b1bc25b729
Bastian Weschasit <bastian.weschasit@iridium-works.com>
2024-06-09 18:13:21 +0200

    when click on a href color should change

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  setActive(event: Event) {
+    let links = document.querySelectorAll('.right-side a');
 
+    links.forEach((link) => {
+      link.classList.remove('active');
+    });
+
+    (event.target as HTMLElement).classList.add('active');
+  }
 }
